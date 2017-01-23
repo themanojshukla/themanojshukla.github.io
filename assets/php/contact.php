fd01b43e45afc949364c4a9c9257417a3bcaba21
themanojshukla <themanojshukla@gmail.com>
2017-01-23 13:51:48 +0530

    Update contact.php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,1 +1,17 @@
-#
+<html>
+  <title>
+    M.K. Shukla
+  </title>
+  <body>
+    Sorry...!!! Service Desabled.<br>
+    We are redirecting you to homepage.
+  </body>
+  <script>
+    $(document).ready(function () {
+    // Handler for .ready() called.
+    window.setTimeout(function () {
+        location.href = "https:/themanojshukla.github.io";
+    }, 5000);
+});
+  </script>
+</html>
